b68f0e4e446519302e4f48986c783654fbb8a1ae
mihajlotimo <e14mihajlotimotijevic@gmail.com>
2026-03-23 21:53:25 +0100

    Init commit

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  template: `<h1>Login Page</h1>`,
+})
+export class Login {}
---
+++ src/app/pages/register/register.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  template: `<h1>Register Page</h1>`,
+})
+export class Register {}
